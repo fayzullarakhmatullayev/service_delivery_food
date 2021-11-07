dc8212593ad38e326fd58fbfce6f4e6ea7d7c80c
Fayzulla Rahmatullayev <80602071+fayzullarakhmatullayev@users.noreply.github.com>
2021-11-07 13:46:06 +0500

    update

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,56 @@
-const restaurant = "food-band";
+const cardsMenu = document.querySelector('.cards-menu');
+const changeTitle = (restaurant) => {
+    const restaurantTitle = document.querySelector('.restaurant-title');
+    restaurantTitle.textContent = restaurant.name;
+    const cardInfo = document.querySelector('.card-info');
+    cardInfo.innerHTML = `
+        <div class="rating">${restaurant.stars}</div>
+        <div class="price">От ${restaurant.price} ₽</div>
+        <div class="category">${restaurant.kitchen}</div>
+    `
+}
 
 const renderItems = data => {
-    data.forEach(({
-        id,
-        description,
-        image,
-        name,
-        price
-    }) => {
-        console.log(id);
-        console.log(description);
-        console.log(image);
-        console.log(name);
-        console.log(price);
+    data.forEach(({id, description, image,  name, price }) => {
+        const card = document.createElement('div')
+        card.classList.add('card');
+        card.innerHTML = `
+            <img src="${image}" alt="${name}" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title card-title-reg">${name}</h3>
+                </div>
+                <!-- /.card-heading -->
+                <div class="card-info">
+                    <div class="ingredients">
+                        ${description}
+                    </div>
+                </div>
+                <!-- /.card-info -->
+                <div class="card-buttons">
+                    <button class="button button-primary button-add-cart">
+                        <span class="button-card-text">В корзину</span>
+                        <span class="button-cart-svg"></span>
+                    </button>
+                    <strong class="card-price-bold">${price} ₽</strong>
+                </div>
+            </div>
+            <!-- /.card-text -->
+        `
+        cardsMenu.append(card)
     });
 }
 
-fetch(`https://delivery-food-22ada-default-rtdb.firebaseio.com/db/${restaurant}.json`)
-    .then(response => response.json())
-    .then(data => renderItems(data))
-    .catch(error => {
-        console.warn(error);
-    })
+if(localStorage.getItem('restaurant')){
+    const restaurant = JSON.parse(localStorage.getItem('restaurant'))
+    changeTitle(restaurant)
+    fetch(`https://delivery-food-22ada-default-rtdb.firebaseio.com/db/${restaurant.products}`)
+        .then(response => response.json())
+        .then(data => renderItems(data))
+        .catch(error => {
+            console.warn(error);
+        })
+} else {
+    window.location.href = '/'
+}
+
--- js/partners.js
+++ js/partners.js
@@ -1,19 +1,42 @@
+const cardsRestaurant = document.querySelector('.cards-restaurants');
+
 const renderItems = data => {
-    data.forEach(({
-        image,
-        kitchen,
-        name,
-        price,
-        stars,
-        time_of_delivery
-    }) => {
-        console.log(image);
-        console.log(kitchen);
-        console.log(name);
-        console.log(price);
-        console.log(stars);
-        console.log(time_of_delivery);
-    });
+    data.forEach(item => {
+        const {image, kitchen, name, price, products, stars, time_of_delivery} = item;
+        const a = document.createElement('a')
+        a.setAttribute('href', '/restaurant.html')
+        a.classList.add('card')
+        a.classList.add('card-restaurant')
+        a.dataset.products = products;
+        a.innerHTML = `
+            <img src="${image}" alt="${name}" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title">${name}</h3>
+                    <span class="card-tag tag">${time_of_delivery} мин</span>
+                </div>
+                <!-- /.card-heading -->
+                <div class="card-info">
+                    <div class="rating">
+                        ${stars}
+                    </div>
+                    <div class="price">${price}</div>
+                    <div class="category">${kitchen}</div>
+                </div>
+                <!-- /.card-info -->
+            </div>
+        `
+        a.addEventListener('click', e => {
+            e.preventDefault()
+            if(localStorage.getItem('user')){
+                localStorage.setItem('restaurant', JSON.stringify(item))
+                window.location.href = '/restaurant.html'
+            } else {
+                document.querySelector('.modal-auth').style.display = 'flex';
+            }
+        })
+        cardsRestaurant.append(a);
+    })
 }
 
 fetch('https://delivery-food-22ada-default-rtdb.firebaseio.com/db/partners.json')
