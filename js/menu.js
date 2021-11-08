80f43c469c852afb4e15250b114b6ca8307c3726
Fayzulla Rahmatullayev <80602071+fayzullarakhmatullayev@users.noreply.github.com>
2021-11-08 10:59:38 +0500

    update

--- js/menu.js
+++ js/menu.js
@@ -1,56 +1,79 @@
-const cardsMenu = document.querySelector('.cards-menu');
-const changeTitle = restaurant => {
-    const restaurantTitle = document.querySelector('.restaurant-title');
-    restaurantTitle.textContent = restaurant.name;
-    const cardInfo = document.querySelector('.card-info');
-    cardInfo.innerHTML = `
-        <div class="rating">${restaurant.stars}</div>
-        <div class="price">От ${restaurant.price} ₽</div>
-        <div class="category">${restaurant.kitchen}</div>
-    `
-}
+const menu = () => {
+    const cardsMenu = document.querySelector('.cards-menu');
 
-const renderItems = data => {
-    data.forEach(({id, description, image,  name, price }) => {
-        const card = document.createElement('div')
-        card.classList.add('card');
-        card.innerHTML = `
-            <img src="${image}" alt="${name}" class="card-image" />
-            <div class="card-text">
-                <div class="card-heading">
-                    <h3 class="card-title card-title-reg">${name}</h3>
-                </div>
-                <!-- /.card-heading -->
-                <div class="card-info">
-                    <div class="ingredients">
-                        ${description}
+    const cartArray = localStorage.getItem('cart') ?
+        JSON.parse(localStorage.getItem('cart')) : [];
+
+    const changeTitle = restaurant => {
+        const restaurantTitle = document.querySelector('.restaurant-title');
+        restaurantTitle.textContent = restaurant.name;
+        const cardInfo = document.querySelector('.card-info');
+        cardInfo.innerHTML = `
+            <div class="rating">${restaurant.stars}</div>
+            <div class="price">От ${restaurant.price} ₽</div>
+            <div class="category">${restaurant.kitchen}</div>
+        `
+    }
+
+    const addToCart = (cartItem) => {
+        if(cartArray.some(item => item.id === cartItem.id)){
+            cartArray.map(item => {
+                if(item.id === cartItem.id){
+                    item.count++
+                }
+                return item
+            })
+        } else {
+            cartArray.push(cartItem)
+            localStorage.setItem('cart', JSON.stringify(cartArray))
+        }
+    }
+
+    const renderItems = data => {
+        data.forEach(({id, description, image,  name, price }) => {
+            const card = document.createElement('div')
+            card.classList.add('card');
+            card.innerHTML = `
+                <img src="${image}" alt="${name}" class="card-image" />
+                <div class="card-text">
+                    <div class="card-heading">
+                        <h3 class="card-title card-title-reg">${name}</h3>
+                    </div>
+                    <!-- /.card-heading -->
+                    <div class="card-info">
+                        <div class="ingredients">
+                            ${description}
+                        </div>
+                    </div>
+                    <!-- /.card-info -->
+                    <div class="card-buttons">
+                        <button class="button button-primary button-add-cart">
+                            <span class="button-card-text">В корзину</span>
+                            <span class="button-cart-svg"></span>
+                        </button>
+                        <strong class="card-price-bold">${price} ₽</strong>
                     </div>
                 </div>
-                <!-- /.card-info -->
-                <div class="card-buttons">
-                    <button class="button button-primary button-add-cart">
-                        <span class="button-card-text">В корзину</span>
-                        <span class="button-cart-svg"></span>
-                    </button>
-                    <strong class="card-price-bold">${price} ₽</strong>
-                </div>
-            </div>
-            <!-- /.card-text -->
-        `
-        cardsMenu.append(card)
-    });
-}
+                <!-- /.card-text -->
+            `
+            card.querySelector('.button-card-text').addEventListener('click', () => {
+                addToCart({ name, price, id, count: 1})
+            })
+            cardsMenu.append(card)
+        });
+    }
 
-if(localStorage.getItem('restaurant')){
-    const restaurant = JSON.parse(localStorage.getItem('restaurant'))
-    changeTitle(restaurant)
-    fetch(`https://delivery-food-22ada-default-rtdb.firebaseio.com/db/${restaurant.products}`)
-        .then(response => response.json())
-        .then(data => renderItems(data))
-        .catch(error => {
-            console.warn(error);
-        })
-} else {
-    window.location.href = '/'
+    if(localStorage.getItem('restaurant')){
+        const restaurant = JSON.parse(localStorage.getItem('restaurant'))
+        changeTitle(restaurant)
+        fetch(`https://delivery-food-22ada-default-rtdb.firebaseio.com/db/${restaurant.products}`)
+            .then(response => response.json())
+            .then(data => renderItems(data))
+            .catch(error => {
+                console.warn(error);
+            })
+    } else {
+        window.location.href = '/'
+    }
 }
-
+menu()
--- js/partners.js
+++ js/partners.js
@@ -1,47 +1,50 @@
-const cardsRestaurant = document.querySelector('.cards-restaurants');
+const partners = () => {
+    const cardsRestaurant = document.querySelector('.cards-restaurants');
 
-const renderItems = data => {
-    data.forEach(item => {
-        const {image, kitchen, name, price, products, stars, time_of_delivery} = item;
-        const a = document.createElement('a')
-        a.setAttribute('href', '/restaurant.html')
-        a.classList.add('card')
-        a.classList.add('card-restaurant')
-        a.dataset.products = products;
-        a.innerHTML = `
-            <img src="${image}" alt="${name}" class="card-image" />
-            <div class="card-text">
-                <div class="card-heading">
-                    <h3 class="card-title">${name}</h3>
-                    <span class="card-tag tag">${time_of_delivery} мин</span>
-                </div>
-                <!-- /.card-heading -->
-                <div class="card-info">
-                    <div class="rating">
-                        ${stars}
+    const renderItems = data => {
+        data.forEach(item => {
+            const {image, kitchen, name, price, products, stars, time_of_delivery} = item;
+            const a = document.createElement('a')
+            a.setAttribute('href', '/restaurant.html')
+            a.classList.add('card')
+            a.classList.add('card-restaurant')
+            a.dataset.products = products;
+            a.innerHTML = `
+                <img src="${image}" alt="${name}" class="card-image" />
+                <div class="card-text">
+                    <div class="card-heading">
+                        <h3 class="card-title">${name}</h3>
+                        <span class="card-tag tag">${time_of_delivery} мин</span>
                     </div>
-                    <div class="price">${price}</div>
-                    <div class="category">${kitchen}</div>
+                    <!-- /.card-heading -->
+                    <div class="card-info">
+                        <div class="rating">
+                            ${stars}
+                        </div>
+                        <div class="price">${price}</div>
+                        <div class="category">${kitchen}</div>
+                    </div>
+                    <!-- /.card-info -->
                 </div>
-                <!-- /.card-info -->
-            </div>
-        `
-        a.addEventListener('click', e => {
-            e.preventDefault()
-            if(localStorage.getItem('user')){
-                localStorage.setItem('restaurant', JSON.stringify(item))
-                window.location.href = '/restaurant.html'
-            } else {
-                document.querySelector('.modal-auth').style.display = 'flex';
-            }
+            `
+            a.addEventListener('click', e => {
+                e.preventDefault()
+                if(localStorage.getItem('user')){
+                    localStorage.setItem('restaurant', JSON.stringify(item))
+                    window.location.href = '/restaurant.html'
+                } else {
+                    document.querySelector('.modal-auth').style.display = 'flex';
+                }
+            })
+            cardsRestaurant.append(a);
         })
-        cardsRestaurant.append(a);
-    })
-}
+    }
 
-fetch('https://delivery-food-22ada-default-rtdb.firebaseio.com/db/partners.json')
-    .then(response => response.json())
-    .then(data => renderItems(data))
-    .catch(error => {
-        console.warn(error);
-    })
+    fetch('https://delivery-food-22ada-default-rtdb.firebaseio.com/db/partners.json')
+        .then(response => response.json())
+        .then(data => renderItems(data))
+        .catch(error => {
+            console.warn(error);
+        })
+}
+partners()
